7b7959612e4e82a0056d3b1cb6c36b43c8728cf3
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 09:01:00 -0500

    check_estimator

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,23 +1,33 @@
-def use_estimator():
+def check_estimator():
 
     import pickle
 
     import pandas as pd
+    from sklearn.metrics import accuracy_score, balanced_accuracy_score
 
-    dataframe = pd.read_csv(
-        "../files/input/sentences.csv.zip",
-        index_col=False,
-        compression="zip",
+    data, target = load_data()
+
+    x_train, x_test, y_train_true, y_test_true = make_train_test_split(
+        x=data,
+        y=target,
     )
-
-    data = dataframe.phrase
 
     with open("estimator.pickle", "rb") as file:
         estimator = pickle.load(file)
 
-    prediction = estimator.predict(data)
+    y_train_pred = estimator.predict(x_train)
+    y_test_pred = estimator.predict(x_test)
 
-    return prediction
+    accuracy_train = round(accuracy_score(y_train_true, y_train_pred), 4)
+    accuracy_test = round(accuracy_score(y_test_true, y_test_pred), 4)
+    balanced_accuracy_train = round(
+        balanced_accuracy_score(y_train_true, y_train_pred), 4
+    )
+    balanced_accuracy_test = round(balanced_accuracy_score(y_test_true, y_test_pred), 4)
+
+    print(estimator.best_estimator_, ":", sep="")
+    print(f"  Balanced Accuracy: {balanced_accuracy_test} ({balanced_accuracy_train})")
+    print(f"           Accuracy: {accuracy_test} ({accuracy_train})")
 
 
-use_estimator()
+check_estimator()
