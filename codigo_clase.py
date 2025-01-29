bf65e100dd67dc4b28f0b683076c4f209af209ca
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 10:29:08 -0500

    def train_logistic_regression():

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,30 +1,24 @@
-def train_estimator(estimator):
+def train_logistic_regression():
 
-    from sklearn.linear_model import LinearRegression
-    from sklearn.metrics import mean_absolute_error
+    from sklearn.linear_model import LogisticRegression
 
-    data, target = load_data()
-
-    x_train, x_test, y_train, y_test = make_train_test_split(
-        x=data,
-        y=target,
+    pipeline = make_pipeline(
+        estimator=LogisticRegression(max_iter=10000, solver="saga"),
     )
 
-    estimator.fit(x_train, y_train)
+    param_grid = {
+        "selectkbest__k": range(1, 11),
+        "estimator__penalty": ["l1", "l2"],
+        "estimator__C": [0.001, 0.01, 0.1, 1, 10, 100],
+    }
 
-    best_estimator = load_estimator()
+    estimator = make_grid_search(
+        estimator=pipeline,
+        param_grid=param_grid,
+        cv=5,
+    )
 
-    if best_estimator is not None:
+    train_estimator(estimator)
 
-        saved_mae = mean_absolute_error(
-            y_true=y_test, y_pred=best_estimator.predict(x_test)
-        )
 
-        current_mae = mean_absolute_error(
-            y_true=y_test, y_pred=estimator.predict(x_test)
-        )
-
-        if saved_mae < current_mae:
-            estimator = best_estimator
-
-    save_estimator(estimator)
+train_logistic_regression()
