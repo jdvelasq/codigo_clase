11ff6092fab9453837c956f05963e0917cdc83cb
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 09:59:09 -0500

    check_estimator

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,6 +1,25 @@
-def report(estimator, mse, mae, r2):
+def check_estimator():
 
-    print(estimator, ":", sep="")
-    print(f"  MSE: {mse}")
-    print(f"  MAE: {mae}")
-    print(f"   R2: {r2}")
+    import pickle
+
+    import pandas as pd
+    from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+    data, target = load_data()
+
+    x_train, x_test, y_train_true, y_test_true = make_train_test_split(
+        x=data,
+        y=target,
+    )
+
+    estimator = load_estimator()
+
+    mse, mae, r2 = eval_metrics(
+        y_test_true,
+        estimator.predict(x_test),
+    )
+
+    report(estimator.best_estimator_, mse, mae, r2)
+
+
+check_estimator()
