198becbeb4a86b83ad60e1842a819d66adeb5b4f
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 08:44:46 -0500

    Update codigo_clase.py

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,15 +1,23 @@
-def train_logistic_regression():
+def use_estimator():
 
-    from sklearn.linear_model import LogisticRegression
+    import pickle
 
-    data, target = load_data()
-    x_train, x_test, y_train, y_test = make_train_test_split(
-        x=data,
-        y=target,
+    import pandas as pd
+
+    dataframe = pd.read_csv(
+        "../files/input/sentences.csv.zip",
+        index_col=False,
+        compression="zip",
     )
-    estimator = make_pipeline(estimator=LogisticRegression(max_iter=1000))
-    estimator.fit(x_train, y_train)
-    save_estimator(estimator)
+
+    data = dataframe.phrase
+
+    with open("estimator.pickle", "rb") as file:
+        estimator = pickle.load(file)
+
+    prediction = estimator.predict(data)
+
+    return prediction
 
 
-train_logistic_regression()
+use_estimator()
