f482196a2e04c9d8cd6f4fbf0ed1d87d17d5a019
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 08:58:04 -0500

    make_grid_search

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,48 +1,12 @@
-def train_logistic_regression():
+def make_grid_search(estimator, param_grid, cv=5):
 
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.metrics import balanced_accuracy_score
+    from sklearn.model_selection import GridSearchCV
 
-    data, target = load_data()
-
-    x_train, x_test, y_train, y_test = make_train_test_split(
-        x=data,
-        y=target,
+    grid_search = GridSearchCV(
+        estimator=estimator,
+        param_grid=param_grid,
+        cv=cv,
+        scoring="balanced_accuracy",
     )
 
-    pipeline = make_pipeline(
-        estimator=LogisticRegression(max_iter=1000),
-    )
-
-    estimator = make_grid_search(
-        estimator=pipeline,
-        param_grid={
-            "transformer__norm": ["l1", "l2", None],
-            "transformer__use_idf": [True, False],
-            "transformer__smooth_idf": [True, False],
-        },
-        cv=5,
-    )
-
-    estimator.fit(x_train, y_train)
-
-    best_estimator = load_estimator()
-
-    if best_estimator is not None:
-
-        saved_balanced_accuracy = balanced_accuracy_score(
-            y_true=y_test, y_pred=best_estimator.predict(x_test)
-        )
-
-        current_balanced_accuracy = balanced_accuracy_score(
-            y_true=y_test, y_pred=estimator.predict(x_test)
-        )
-
-        if current_balanced_accuracy < saved_balanced_accuracy:
-            estimator = best_estimator
-
-    save_estimator(estimator)
-
-
-train_logistic_regression()
-
+    return grid_search
