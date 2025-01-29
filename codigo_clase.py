dd4917830690f1e63be92993da171ca3efb6354f
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 09:55:05 -0500

    train_linear_regression

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,11 +1,46 @@
-def load_estimator():
+def train_linear_regression():
 
-    import os
-    import pickle
+    from sklearn.linear_model import LinearRegression
+    from sklearn.metrics import mean_absolute_error
+    
 
-    if not os.path.exists("estimator.pickle"):
-        return None
-    with open("estimator.pickle", "rb") as file:
-        estimator = pickle.load(file)
+    data, target = load_data()
 
-    return estimator
+    x_train, x_test, y_train, y_test = make_train_test_split(
+        x=data,
+        y=target,
+    )
+
+    pipeline = make_pipeline(
+        estimator=LinearRegression(),
+    )
+
+    estimator = make_grid_search(
+        estimator=pipeline,
+        param_grid={
+            "selectkbest__k": range(1, len(x_train.columns) + 1),
+        },
+        cv=5,
+    )
+
+    estimator.fit(x_train, y_train)
+
+    best_estimator = load_estimator()
+
+    if best_estimator is not None:
+
+        saved_mae = mean_absolute_error(
+            y_true=y_test, y_pred=best_estimator.predict(x_test)
+        )
+
+        current_mae = mean_absolute_error(
+            y_true=y_test, y_pred=estimator.predict(x_test)
+        )
+
+        if saved_mae < current_mae:
+            estimator = best_estimator
+
+    save_estimator(estimator)
+
+
+train_linear_regression()
