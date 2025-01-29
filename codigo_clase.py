71ba4e812834b095edc954cb4282a0225a6b12fd
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 09:45:42 -0500

    make_pipeline

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,11 +1,27 @@
-def make_train_test_split(x, y):
+def make_pipeline(estimator):
 
-    from sklearn.model_selection import train_test_split
+    from sklearn.compose import ColumnTransformer
+    from sklearn.feature_selection import SelectKBest, f_regression
+    from sklearn.pipeline import Pipeline
+    from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
-    (x_train, x_test, y_train, y_test) = train_test_split(
-        x,
-        y,
-        test_size=0.25,
-        random_state=123456,
+    transformer = ColumnTransformer(
+        transformers=[
+            ("ohe", OneHotEncoder(dtype="int"), ["Origin"]),
+        ],
+        remainder=StandardScaler(),
     )
-    return x_train, x_test, y_train, y_test
+
+    selectkbest = SelectKBest(score_func=f_regression)
+
+    pipeline = Pipeline(
+        steps=[
+            ("tranformer", transformer),
+            ("selectkbest", selectkbest),
+            ("estimator", estimator),
+        ],
+        verbose=False,
+    )
+
+    return pipeline
+
