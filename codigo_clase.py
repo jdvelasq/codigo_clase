5e65dfb0003715a0bca08e322656a0daeb1f74dc
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 10:19:41 -0500

    Taller 3 - load_data

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,20 +1,17 @@
-```
+def load_data():
 
-Column     Description                                             Feature Type
----------------------------------------------------------------------------------
-Age        Age in years                                            Numerical
-Sex        (1 = male; 0 = female)                                  Categorical
-CP         Chest pain type (0, 1, 2, 3, 4)                         Categorical
-Trestbpd   Resting blood pressure (in mm Hg on admission)          Numerical  
-Chol       Serum cholesterol in mg/dl                              Numerical
-FBS        fasting blood sugar in 120 mg/dl (1 = true; 0 = false)  Categorical
-RestECG    Resting electrocardiogram results (0, 1, 2)             Categorical 
-Thalach    Maximum heart rate achieved                             Numerical
-Exang      Exercise induced angina (1 = yes; 0 = no)               Categorical
-Oldpeak    ST depression induced by exercise relative to rest      Numerical
-Slope      Slope of the peak exercise ST segment                   Numerical
-CA         Number of major vessels (0-3) colored by fluoroscopy    Categorical
-Thal       3 = normal; 6 = fixed defect; 7 = reversible defect     Categorical
-Target     Diagnosis of heart disease (1 = true; 0 = false)        Target
+    import pandas as pd
 
-```
+    dataset = pd.read_csv("heart_disease.csv")
+    y = dataset.pop("target")
+    x = dataset.copy()
+    x["thal"] = x["thal"].map(
+        lambda x: "normal" if x not in ["fixed", "fixed", "reversible"] else x
+    )
+
+    return x, y
+
+
+x, y = load_data()
+x
+
