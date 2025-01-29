17c38c6ea119d8eaf301c3a4dd3fe4792fbead3f
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 08:51:42 -0500

    Update codigo_clase.py

--- codigo_clase.py
+++ codigo_clase.py
@@ -4,3 +4,16 @@ def save_estimator(estimator):
 
     with open("estimator.pickle", "wb") as file:
         pickle.dump(estimator, file)
+
+
+def load_estimator():
+
+    import os
+    import pickle
+
+    if not os.path.exists("estimator.pickle"):
+        return None
+    with open("estimator.pickle", "rb") as file:
+        estimator = pickle.load(file)
+
+    return estimator
