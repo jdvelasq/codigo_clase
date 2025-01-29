da2d1156af9080a61b51de20ddc35bf704eb3881
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 10:02:19 -0500

    train_mlp_regressor

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,25 +1,49 @@
-def check_estimator():
+def train_mlp_regressor():
 
-    import pickle
-
-    import pandas as pd
-    from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+    from sklearn.neural_network import MLPRegressor
+    from sklearn.metrics import mean_absolute_error
 
     data, target = load_data()
 
-    x_train, x_test, y_train_true, y_test_true = make_train_test_split(
+    x_train, x_test, y_train, y_test = make_train_test_split(
         x=data,
         y=target,
     )
 
-    estimator = load_estimator()
-
-    mse, mae, r2 = eval_metrics(
-        y_test_true,
-        estimator.predict(x_test),
+    pipeline = make_pipeline(
+        estimator=MLPRegressor(max_iter=30000),
     )
 
-    report(estimator.best_estimator_, mse, mae, r2)
+    estimator = make_grid_search(
+        estimator=pipeline,
+        param_grid={
+            "selectkbest__k": range(1, len(x_train.columns) + 1),
+            "estimator__hidden_layer_sizes": [(n,) for n in range(1, 11)],
+            "estimator__solver": ["adam"],
+            "estimator__learning_rate_init": [0.01, 0.001, 0.0001],
+        },
+        cv=5,
+    )
+
+    estimator.fit(x_train, y_train)
+
+    best_estimator = load_estimator()
+
+    if best_estimator is not None:
+
+        saved_mae = mean_absolute_error(
+            y_true=y_test, y_pred=best_estimator.predict(x_test)
+        )
+
+        current_mae = mean_absolute_error(
+            y_true=y_test, y_pred=estimator.predict(x_test)
+        )
+
+        if saved_mae < current_mae:
+            estimator = best_estimator
+
+    save_estimator(estimator)
 
 
+train_mlp_regressor()
 check_estimator()
