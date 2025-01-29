205273f95e417fa751ae684b88a199c07808e377
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 09:57:25 -0500

    eval_metrics

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,46 +1,9 @@
-def train_linear_regression():
+def eval_metrics(y_true, y_pred):
 
-    from sklearn.linear_model import LinearRegression
-    from sklearn.metrics import mean_absolute_error
-    
+    from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-    data, target = load_data()
+    mse = mean_squared_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+    r2 = r2_score(y_true, y_pred)
 
-    x_train, x_test, y_train, y_test = make_train_test_split(
-        x=data,
-        y=target,
-    )
-
-    pipeline = make_pipeline(
-        estimator=LinearRegression(),
-    )
-
-    estimator = make_grid_search(
-        estimator=pipeline,
-        param_grid={
-            "selectkbest__k": range(1, len(x_train.columns) + 1),
-        },
-        cv=5,
-    )
-
-    estimator.fit(x_train, y_train)
-
-    best_estimator = load_estimator()
-
-    if best_estimator is not None:
-
-        saved_mae = mean_absolute_error(
-            y_true=y_test, y_pred=best_estimator.predict(x_test)
-        )
-
-        current_mae = mean_absolute_error(
-            y_true=y_test, y_pred=estimator.predict(x_test)
-        )
-
-        if saved_mae < current_mae:
-            estimator = best_estimator
-
-    save_estimator(estimator)
-
-
-train_linear_regression()
+    return mse, mae, r2
