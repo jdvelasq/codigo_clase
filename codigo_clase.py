d67be29f14b6eb77f58d301846c04491108e2a5c
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 08:31:54 -0500

    Update codigo_clase.py

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,71 +1,23 @@
-def train_estimator():
+def use_estimator():
 
-    import pandas as pd
-    from sklearn.feature_extraction.text import TfidfTransformer
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.model_selection import train_test_split
-    from sklearn.pipeline import Pipeline
-    from sklearn.metrics import balanced_accuracy_score
-    from sklearn.feature_extraction.text import CountVectorizer
-    from sklearn.metrics import accuracy_score, balanced_accuracy_score
-
-    # import os
     import pickle
 
-    #
-    # Manejo de la data
-    #
+    import pandas as pd
+
     dataframe = pd.read_csv(
-        "../files/input/sentences.csv.zip",
+        "sentences.csv.zip",
         index_col=False,
         compression="zip",
     )
 
     data = dataframe.phrase
-    target = dataframe.target
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        data,
-        target,
-        test_size=0.3,
-        shuffle=False,
-    )
+    with open("estimator.pickle", "rb") as file:
+        estimator = pickle.load(file)
 
-    #
-    # Modelo de regresión logística
-    #
-    vectorizer = CountVectorizer(
-        lowercase=True,
-        analyzer="word",
-        token_pattern=r"\b[a-zA-Z]\w+\b",
-        stop_words="english",
-    )
+    prediction = estimator.predict(data)
 
-    transformer = TfidfTransformer()
-
-    lr_estimator = Pipeline(
-        steps=[
-            ("vectorizer", vectorizer),
-            ("transformer", transformer),
-            ("estimator", LogisticRegression(max_iter=1000)),
-        ],
-        verbose=False,
-    )
-
-    lr_estimator.fit(X_train, y_train)
-
-    with open("estimator.pickle", "wb") as file:
-        pickle.dump(lr_estimator, file)
+    return prediction
 
 
-train_estimator()
-
-
-
-
-
-
-
-
-
-
+use_estimator()
