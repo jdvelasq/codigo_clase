f60834032b3ac4236af9859fd0afafaed0c1c55c
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 08:35:49 -0500

    Update codigo_clase.py

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,23 +1,14 @@
-def use_estimator():
-
-    import pickle
+def load_data():
 
     import pandas as pd
 
     dataframe = pd.read_csv(
-        "../files/input/sentences.csv.zip",
+        "sentences.csv.zip",
         index_col=False,
         compression="zip",
     )
 
     data = dataframe.phrase
+    target = dataframe.target
 
-    with open("estimator.pickle", "rb") as file:
-        estimator = pickle.load(file)
-
-    prediction = estimator.predict(data)
-
-    return prediction
-
-
-use_estimator()
+    return data, target
