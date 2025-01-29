b36a008d60ee00bcc75d6a02c9a20607cdf10290
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 08:52:46 -0500

    Update codigo_clase.py

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,19 +1,48 @@
-def save_estimator(estimator):
+def train_logistic_regression():
 
-    import pickle
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.metrics import balanced_accuracy_score
 
-    with open("estimator.pickle", "wb") as file:
-        pickle.dump(estimator, file)
+    data, target = load_data()
+
+    x_train, x_test, y_train, y_test = make_train_test_split(
+        x=data,
+        y=target,
+    )
+
+    pipeline = make_pipeline(
+        estimator=LogisticRegression(max_iter=1000),
+    )
+
+    estimator = make_grid_search(
+        estimator=pipeline,
+        param_grid={
+            "transformer__norm": ["l1", "l2", None],
+            "transformer__use_idf": [True, False],
+            "transformer__smooth_idf": [True, False],
+        },
+        cv=5,
+    )
+
+    estimator.fit(x_train, y_train)
+
+    best_estimator = load_estimator()
+
+    if best_estimator is not None:
+
+        saved_balanced_accuracy = balanced_accuracy_score(
+            y_true=y_test, y_pred=best_estimator.predict(x_test)
+        )
+
+        current_balanced_accuracy = balanced_accuracy_score(
+            y_true=y_test, y_pred=estimator.predict(x_test)
+        )
+
+        if current_balanced_accuracy < saved_balanced_accuracy:
+            estimator = best_estimator
+
+    save_estimator(estimator)
 
 
-def load_estimator():
+train_logistic_regression()
 
-    import os
-    import pickle
-
-    if not os.path.exists("estimator.pickle"):
-        return None
-    with open("estimator.pickle", "rb") as file:
-        estimator = pickle.load(file)
-
-    return estimator
