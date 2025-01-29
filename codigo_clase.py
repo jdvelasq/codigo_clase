458057e3338525a1e58ba0b90e7e6288e3da181a
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 09:43:27 -0500

    load_data

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,60 +1,16 @@
-def save_estimator(estimator):
-
-    import pickle
-
-    with open("estimator.pickle", "wb") as file:
-        pickle.dump(estimator, file)
-
-
-
-
-def train_logistic_regression():
-
-    from sklearn.linear_model import LogisticRegression
-
-    data, target = load_data()
-    x_train, x_test, y_train, y_test = make_train_test_split(
-        x=data,
-        y=target,
-    )
-    estimator = make_pipeline(estimator=LogisticRegression(max_iter=1000))
-    estimator.fit(x_train, y_train)
-    save_estimator(estimator)
-
-
-train_logistic_regression()
-
-
-
-
-def use_estimator():
-
-    import pickle
+def load_data():
 
     import pandas as pd
 
-    dataframe = pd.read_csv(
-        "../files/input/sentences.csv.zip",
-        index_col=False,
-        compression="zip",
+    dataset = pd.read_csv("../files/input/auto_mpg.csv")
+    dataset = dataset.dropna()
+    dataset["Origin"] = dataset["Origin"].map(
+        {1: "USA", 2: "Europe", 3: "Japan"},
     )
+    y = dataset.pop("MPG")
+    x = dataset.copy()
 
-    data = dataframe.phrase
-
-    with open("estimator.pickle", "rb") as file:
-        estimator = pickle.load(file)
-
-    prediction = estimator.predict(data)
-
-    return prediction
-
-
-use_estimator()
-
-
-
-
-
+    return x, y
 
 
 
