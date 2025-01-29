6ad2284d9493aad01f6be4a575c113bd2dfac142
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-29 09:57:59 -0500

    report

--- codigo_clase.py
+++ codigo_clase.py
@@ -1,9 +1,6 @@
-def eval_metrics(y_true, y_pred):
+def report(estimator, mse, mae, r2):
 
-    from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-
-    mse = mean_squared_error(y_true, y_pred)
-    mae = mean_absolute_error(y_true, y_pred)
-    r2 = r2_score(y_true, y_pred)
-
-    return mse, mae, r2
+    print(estimator, ":", sep="")
+    print(f"  MSE: {mse}")
+    print(f"  MAE: {mae}")
+    print(f"   R2: {r2}")
